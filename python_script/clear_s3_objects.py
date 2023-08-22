ad3f7bde95f65e52bb8408833af91eca6b2407f2
Nguyen Anh <anhngoctuan.nguyen@dxc.com>
2023-08-22 13:40:04 +0700

    update 011

--- python_script/clear_s3_objects.py
+++ python_script/clear_s3_objects.py
@@ -1,27 +1,23 @@
 import boto3
 
-# Thay đổi region thành region của bucket của bạn
 region = 'ap-southeast-1'
-
-# Danh sách các tên bucket cần xóa
 bucket_names = ["aaa-nntanh76-s3bucket01", "aaa-nntanh76-s3bucket02", "aaa-nntanh76-s3bucket03"]
 
-# Tạo một phiên làm việc với S3
+# choose s3 resouce to work
 s3 = boto3.client('s3', region_name=region)
 
-# Lặp qua từng bucket và xóa các đối tượng trong đó
 for bucket_name in bucket_names:
-    # Lấy danh sách các đối tượng trong bucket
+    # get bucket_names list
     objects = s3.list_objects_v2(Bucket=bucket_name)
 
-    # Xóa từng đối tượng
+    # remove each object
     for obj in objects.get('Contents', []):
         object_key = obj['Key']
         s3.delete_object(Bucket=bucket_name, Key=object_key)
 
     print(f"Deleted {len(objects)} objects in {bucket_name}.")
 
-    # Xóa bucket sau khi xóa hết đối tượng
+    # remove all s3 buckets
     # s3.delete_bucket(Bucket=bucket_name)
 
 print("All specified buckets and their contents have been deleted.")
